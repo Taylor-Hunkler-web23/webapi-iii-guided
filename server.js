4e5f57d206a57a6e8eb3566f6419eec02e6d3f1f
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-30 13:38:35 -0400

    Added else statment for password

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express'); // importing a CommonJS module
 
-const helmet = require('helmet')
+const helmet = require('helmet');
+
+const morgan = require ('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
 //
-function log (req, res, next) {
-  console.log('Request Type:', req.method)
-  next()
-}
+// function log (req, res, next) {
+//   console.log('Request Type:', req.method)
+//   next()
+// }
 
 //the three amigos
 // function dateLogger(req, res, next){
@@ -20,9 +22,37 @@ function log (req, res, next) {
 // }
 
 //global middleware
+
+
+// function gateKeeper(req, res, next) {
+  // data can come in the body, url parameters, query string, headers
+  // new way of reading data sent by the client
+
+
+  // change the gatekeeper to return a 400 if no password is provided and a message
+// that says please provide a password
+// if a password is provided and it is mellon, call next, otherwise return a 401
+// and the you shall not pass message
+function gateKeeper(req, res, next) {
+  // data can come in the body, url parameters, query string, headers
+  // new way of reading data sent by the client
+  const password = req.headers.password || '';
+  if (password.toLowerCase() === 'mellon') {
+    next();
+  } 
+  else if(password === ''){
+  res.status(400).json({ you: 'please provide a password' });
+}
+  else  {
+    res.status(401).json({ you: 'shall not pass' });
+  }
+}
+
+
 server.use(helmet());// third party
 server.use(express.json());//built in
-
+server.use(gateKeeper);
+server.use(morgan('dev'));
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
